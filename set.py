151bb1eeae85cf5dd9c862333699dfef4ac5d40c
N.Nandan kumar <nandunandan593@gmail.com>
2025-09-24 10:02:14 +0530

    add

---
+++ set.py
@@ -0,0 +1,4 @@
+n={1,2,3,4}
+c={4,5,2,6,7}
+print(n.union(c))
+print(n.intersection(c))
--- str.py
+++ str.py
@@ -7,7 +7,7 @@ print(type(str))
 #print(list)
 #r=reversed(list)
 #r=list[::-1]'''
-list=["MONICA","nagarjuna","soubin","rajini","bokkodu"]
+'''list=["MONICA","nagarjuna","soubin","rajini","bokkodu"]
 print(list)
 list.append("upendra")
 print(list)
@@ -16,4 +16,11 @@ print(list)
 list.append("amirkhan")
 print(list)
 list.remove("nagarjuna")
-print(list)
+print(list)'''
+list=[]
+n=int(input("enter the sizee of list"))
+for d in range(1,n+1):
+    data=input("enter the data")
+    list.append(data)
+for i in list:
+    print(i)
