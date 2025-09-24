11b1597522a16c53fe777f2b0bcf855780a1f90b
N.Nandan kumar <nandunandan593@gmail.com>
2025-09-24 10:04:10 +0530

    add

--- str.py
+++ str.py
@@ -1,22 +1,3 @@
-#str="n"
-'''print(id(str))
-str1="n"
-print(id(str1))
-print(type(str))
-
-#print(list)
-#r=reversed(list)
-#r=list[::-1]'''
-'''list=["MONICA","nagarjuna","soubin","rajini","bokkodu"]
-print(list)
-list.append("upendra")
-print(list)
-list.insert(4,"sruthihasaan")
-print(list)
-list.append("amirkhan")
-print(list)
-list.remove("nagarjuna")
-print(list)'''
 list=[]
 n=int(input("enter the sizee of list"))
 for d in range(1,n+1):
